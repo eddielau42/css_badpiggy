e19394b5994e7ab80e5b532d03cb767bd70851d1
eddielau42 <97686145+eddielau42@users.noreply.github.com>
2024-05-09 14:51:47 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 (function main() {
   const STYLE_PIXEL = 'pixel';
-  const switcher = document.querySelector('.switcher');
-  if (switcher) {
-    switcher.addEventListener('click', () => {
-      document.body.classList.toggle(STYLE_PIXEL);
-    });
-  }
+
+  const styleSwitch = () => {
+    document.body.classList.toggle(STYLE_PIXEL);
+  };
+  
+  document.querySelector('.switcher').addEventListener('click', styleSwitch);
+  document.querySelector('.piggy .nose').addEventListener('click', styleSwitch);
+  
 })();
